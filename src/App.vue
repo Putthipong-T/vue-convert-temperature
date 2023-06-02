b1ec78071c87fd5d17147de29f3621232884c9a2
Putthipong Tiamduangtawan <pud987@gmail.com>
2023-06-02 23:37:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="bg-gradient-to-tr from-gray-700 to-gray-900 min-w-screen min-h-screen">
+		<div class="flex justify-center text-white text-6xl pt-10">
+		    Convert Celsius(°C) to Fahrenheit(°F)
+		</div>
+		
+		<div id="Body" class="pt-10 flex flex-col items-center justify-center w-auto">
+            <form>
+                <label for="celsius_input" class="text-white -mb-11 text-xl">Enter temperature (°C)</label><br>
+                <input v-model="celsius" type='number' id="celsius_input" onkeydown="return event.keyCode !== 69" class="bg-slate-300 my-4 text-3xl rounded-lg px-3 py-2"  placeholder="Enter temperature (°C)" required/>
+            </form>
+			<button class="bg-gradient-to-tr from-cyan-500 to-blue-500 hover:bg-gradient-to-tr hover:from-cyan-700 hover:to-blue-700 text-white font-bold py-2 px-4 rounded-lg w-fit" v-on:click="ToggleModal()">
+                Calculate
+			</button>
+            <Modal v-if="openModal.buttonTrigger" :Toggle ="() => ToggleModal()" >
+                <h2>{{celsius}}°C ≈ {{fahrenheit}}°F</h2>
+            </Modal>
+            <Modal v-if="openModal.valueTrigger" :Toggle ="() => ToggleModal()" >
+                <h2 class="text-red-500">Please enter the value (°C)</h2>
+            </Modal>
+
+		</div>
+		
+		
+	</div>
+</template>
+
+<script lang="ts">
+    import { ref } from 'vue';
+	import Modal from './components/icons/Popup.vue';
+
+	export default {
+        name: 'App',
+        data() {
+            return {
+                celsius : null
+            }
+        },
+        setup(){
+            const celsius = ref<number | null>(null)
+            const fahrenheit = ref<number | null>(null);
+            const isNumber = (value: unknown): value is number => {return typeof value === "number" && !isNaN(value);};
+            const convert = (() => {
+                if (isNumber(celsius.value)) {
+                    let F = (celsius.value * 9) / 5 + 32;
+                    fahrenheit.value = Number(F.toFixed(2));
+                    console.log('hi');
+                } else {
+                    fahrenheit.value = null;
+                }
+                // if (typeof(celsius.value) === 'number') {
+                //     let F = (celsius.value * 9) / 5 + 32
+                //     fahrenheit.value = Number(F.toFixed(2))
+                //     console.log('qwe')
+                // }else{
+                //     fahrenheit.value = null
+                    
+                // }
+            })
+            const openModal = ref({ buttonTrigger:false, valueTrigger:false});
+            const ToggleModal = (() => {
+
+                convert()
+                openModal.value.buttonTrigger = !openModal.value.buttonTrigger;
+                if(fahrenheit.value === null){
+                    openModal.value.buttonTrigger = !openModal.value.buttonTrigger
+                    openModal.value.valueTrigger = !openModal.value.valueTrigger
+                }
+            })
+     
+
+            return {fahrenheit, celsius, convert, openModal, ToggleModal}
+        },
+        components: {
+            Modal,
+        },
+	};
+</script>
+
+
---
+++ src/components/icons/Popup.vue
@@ -0,0 +1,43 @@
+<template>
+        <div class ="modal flex content-center items-center fixed inset-0 bg-black bg-opacity-70 transition-opacity duration-400 ease-in-out">
+            <div class="modal-content w-auto h-auto bg-white m-auto text-center rounded-t-2xl rounded-b-2xl">
+                <div class="modal-header flex justify-between bg-slate-500 rounded-t-xl p-5">
+                    <h1 class="text-3xl text-white font-bold">Convert Celsius to Fahrenheit</h1>
+                </div>
+                <div class="modal-body" >
+                    <div class="text-4xl font-semibold pt-3 px-5">
+                        <slot class=""/>
+                    </div>
+                    <button class="bg-gradient-to-tr from-cyan-500 to-blue-500 hover:bg-gradient-to-tr hover:from-cyan-700 hover:to-blue-700 text-white font-bold py-2 px-4 my-4 rounded w-52" v-on:click = Toggle>
+                        Close
+                    </button>
+                </div>
+            </div>
+            
+
+        </div>
+    <!-- </div> -->
+</template>
+
+<script lang="ts">
+    import { ref } from 'vue';
+
+export default {
+    name: "Modal",
+    props: ['Toggle'],
+    setup(){
+            const openModal = ref({ buttonTrigger:false});
+            const ToggleModal = (() => {
+                openModal.value.buttonTrigger = !openModal.value.buttonTrigger;
+                console.log(openModal.value.buttonTrigger)
+            })
+     
+
+            return {openModal, ToggleModal}
+        },
+};
+</script>
+
+<style scoped>
+
+</style>
