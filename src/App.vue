4a5ac95bc88179a44dd27923334da7d4b6c61181
Putthipong Tiamduangtawan <pud987@gmail.com>
2023-06-03 00:15:43 +0700

    add form with onsubmit

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,12 @@
             <form onsubmit="return false">
                 <label for="celsius_input" class="text-white -mb-11 text-xl">Enter temperature (°C)</label><br>
                 <input v-model="celsius" type='number' id="celsius_input" onkeydown="return event.keyCode !== 69" class="bg-slate-300 my-4 text-3xl rounded-lg px-3 py-2"  placeholder="Enter temperature (°C)" required/>
-			<button class="bg-gradient-to-tr from-cyan-500 to-blue-500 hover:bg-gradient-to-tr hover:from-cyan-700 hover:to-blue-700 text-white font-bold py-2 px-4 rounded-lg w-fit" v-on:click="ToggleModal()">
-                Calculate
-			</button>
+                <br>
+                <div class="flex justify-center">
+                    <button class="bg-gradient-to-tr from-cyan-500 to-blue-500 hover:bg-gradient-to-tr hover:from-cyan-700 hover:to-blue-700 text-white font-bold py-2 px-4 rounded-lg w-fit" v-on:click="ToggleModal()">
+                        Calculate
+                    </button>
+                </div>
             </form>
             <Modal v-if="openModal.buttonTrigger" :Toggle ="() => ToggleModal()" >
                 <h2>{{celsius}}°C ≈ {{fahrenheit}}°F</h2>
